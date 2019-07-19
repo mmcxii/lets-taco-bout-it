f2ae7dbc5a43d6ed831325675fb53a538b45d793
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-07-18 20:07:50 -0700

    Hybrid button working

--- assets/js/Modal.js
+++ assets/js/Modal.js
@@ -1,3 +1,5 @@
+let hybrid = false;
+
 const modal = document.querySelector('#yelp-form-modal');
 const closeModalBtn = document.querySelector('#yelp-form-close');
 const optionsBtns = document.querySelector('#options-btns');
@@ -7,12 +9,18 @@ optionsBtns.addEventListener('click', openModal);
 window.addEventListener('click', outsideClick);
 
 function closeModal() {
+    hybrid = false;
     modal.classList.add('hide');
 }
 
 function openModal(e) {
-    if (e.target.id === 'restaurant-btn' || e.target.id === 'hybrid-btn') {
+    if (e.target.id === 'restaurant-btn') {
         modal.classList.remove('hide');
+    }
+
+    if (e.target.id === 'hybrid-btn') {
+        modal.classList.remove('hide');
+        hybrid = true;
     }
 }
 
